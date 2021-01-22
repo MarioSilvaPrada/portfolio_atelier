f9a5848a280903177fa922ac0f2de0a55ea63ce3
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-01-22 23:03:45 +0000

    added ListView

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Project(models.Model):
+
+    author = models.TextField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.author
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Project
 
 
-class HomePageView(TemplateView): 
-    
+class HomePageView(ListView):
+    model = Project
     template_name = 'home.html'
 
 
-class AboutPageView(TemplateView): 
-    
+class AboutPageView(ListView):
+    model = Project
     template_name = 'about.html'
